9eed42256838b3ef8676f5af65e8ad1b621fbb7b
Marco Morelos <mmore019@bolt.cs.ucr.edu>
2017-04-13 11:27:36 -0700

    Moved everything out of Phase1 folder. Added .gitignore Added main method with basic parser.

---
+++ Typecheck.java
@@ -0,0 +1,19 @@
+import syntaxtree.*;
+import visitor.*;
+
+class Typecheck{
+  public static void main(String args[]){
+    //taken from JTB tutorial
+    MiniJavaParser parser = new MiniJavaParser(System.in);
+
+    try {
+      Node root = parser.Goal();
+      System.err.println("Java program parsed successfully.");
+      root.accept(new DepthFirstVisitor());
+    }
+    catch (ParseException e) {
+      System.err.println("Encountered errors during parse.");
+    }
+
+  }
+}
