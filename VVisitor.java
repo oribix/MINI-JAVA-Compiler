15956bbfda22f87a2819bb40ce8e126d957164d5
Marco Morelos <mmore019@bolt.cs.ucr.edu>
2017-06-01 10:54:50 -0700

    modified:   VVisitor.java
    implemented VBranch, VMemWrite, VMemRead

--- VVisitor.java
+++ VVisitor.java
@@ -4,6 +4,7 @@ import cs132.vapor.ast.VBuiltIn;
 import cs132.vapor.ast.VCall;
 import cs132.vapor.ast.VGoto;
 import cs132.vapor.ast.VMemRead;
+import cs132.vapor.ast.VMemRef;
 import cs132.vapor.ast.VMemWrite;
 import cs132.vapor.ast.VReturn;
 import cs132.vapor.ast.VInstr.VisitorPR;
@@ -17,6 +18,14 @@ public class VVisitor extends
   }
   
   public String visit(String s, VBranch b) throws RuntimeException {
+    String ifString = "";
+    if(b.positive)
+      ifString = "if";
+    else
+      ifString = "if0";
+
+    System.out.println(ifString + " " + b.value + " goto " + b.target);
+
     return "VBranch";
   }
   
@@ -29,14 +38,33 @@ public class VVisitor extends
   }
   
   public String visit(String s, VGoto g) throws RuntimeException {
+    System.out.println("goto " + g.target);
     return "VGoto";
   }
   
   public String visit(String s, VMemRead r) throws RuntimeException {
+    String base = ((VMemRef.Global)r.source).base.toString();
+    int byteOffset = ((VMemRef.Global)r.source).byteOffset;
+
+    if(base.equals("this") && byteOffset == 0)
+      System.out.println(r.dest + " = [this]");
+    else
+      System.out.println(r.dest + " = [" + base + " + " +  byteOffset + "]");
+
     return "VMemRead";
   }
   
   public String visit(String s, VMemWrite w) throws RuntimeException {
+    String base = ((VMemRef.Global)w.dest).base.toString();
+    int byteOffset = ((VMemRef.Global)w.dest).byteOffset;
+
+    String src = w.source.toString();
+
+    if(byteOffset == 0)
+      System.out.println("[" + base + "] = " + src);
+    else
+      System.out.println("[" + base + " + " + byteOffset + "] = " + src);
+
     return "VMemWrite";
   }
   
