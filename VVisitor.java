8c4d22f69d995f430a9dcf5315c76e3d05485195
Harry Chu <hchu007@bolt.cs.ucr.edu>
2017-06-01 10:49:35 -0700

    builtin, call, return

--- VVisitor.java
+++ VVisitor.java
@@ -7,6 +7,8 @@ import cs132.vapor.ast.VMemRead;
 import cs132.vapor.ast.VMemWrite;
 import cs132.vapor.ast.VReturn;
 import cs132.vapor.ast.VInstr.VisitorPR;
+import cs132.vapor.ast.VOperand;
+import cs132.vapor.ast.VMemRef;
 
 public class VVisitor extends 
   VisitorPR<String, String, RuntimeException> {
@@ -21,10 +23,36 @@ public class VVisitor extends
   }
   
   public String visit(String s, VBuiltIn c) throws RuntimeException {
+    String code = new String();
+    if(c.dest != null)
+      code = c.dest + " = " + c.op.name + "(";
+    else
+      code = c.op.name + "(";
+    String argz = new String();
+    for(VOperand arg : c.args) {
+      argz = argz + " " + arg.toString();
+    }
+    argz = argz.substring(1);
+    code += argz;
+    code += ")";
+    System.out.println(code);
     return "VBuiltIn";
   }
   
   public String visit(String s, VCall c) throws RuntimeException {
+    String code = new String();
+    if(c.dest != null)
+      code = c.dest + " = call " + c.addr + "(";
+    else
+      code = c.addr + "(";
+    String argz = new String();
+    for(VOperand arg : c.args) {
+      argz = argz + " " + arg.toString();
+    }
+    argz = argz.substring(1);
+    code += argz;
+    code += ")";
+    System.out.println(code);
     return "VCall";
   }
   
@@ -33,14 +61,22 @@ public class VVisitor extends
   }
   
   public String visit(String s, VMemRead r) throws RuntimeException {
+    // System.out.println(r.dest + " = " + "[" + ((VMemRef.Global)r.source).base + "]");
     return "VMemRead";
   }
   
   public String visit(String s, VMemWrite w) throws RuntimeException {
+    // System.out.println("[" + ((VMemRef.Global)w.dest).base + "] = " + w.source);
     return "VMemWrite";
   }
   
   public String visit(String s, VReturn r) throws RuntimeException {
+    if(r.value == null) {
+      System.out.println("ret");
+    }
+    else {
+      System.out.println("ret"); //store return value in $v0
+    }
     return "VReturn";
   }
 }
